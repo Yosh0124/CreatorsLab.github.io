fdaee81bf3b14d138c7df84e26eac345283e6b92
yuta_nishimori <hunger_yn@icloud.com>
2015-08-26 22:15:15 +0900

    [マージミスを修正]ヘッダー、フッダーの共通化完了 #12

--- js/template.js
+++ js/template.js
@@ -1,39 +1,9 @@
 
 
 function header(){
-<<<<<<< HEAD
 
 
     var html = (function() {/*
-    
-=======
-    var html = "";
-    html +='<nav class="navbar navbar-default navbar-fixed-top">';
-    html +='<div class="container-fluid">';
-    html +='<!-- Brand and toggle get grouped for better mobile display -->';
-    html +='<div class="navbar-header">';
-    html +='<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">';
-    html +='<span class="sr-only">Toggle navigation</span>';
-    html +='<span class="icon-bar"></span>';
-    html +='<span class="icon-bar"></span>';
-    html +='<span class="icon-bar"></span>';
-    html +='</button>';
-    html +='<a class="navbar-brand brand-title" href="index.html">iloli</a>';
-    html +='</div>';
-    html +='<!-- Collect the nav links, forms, and other content for toggling -->';
-    html +='<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">';
-    html +='<div class="navbar-right">';
-    html +='<ul class="nav navbar-nav">';
-    html +='<li><a href="#whats">iloliとは</a></li>';
-    html +='</ul>';
-    html +='<a class="btn btn-success navbar-btn" href="https://docs.google.com/forms/d/1jXNnrTf5IUhFlovefWSTonEIExJvdZVld7nfWpY9tI8/viewform" role="button" target=”_blank”>お問い合わせ</a>';
-    html +='</div>';
-    html +='</div><!-- /.navbar-collapse -->';
-    html +='</div><!-- /.container-fluid -->';
-    html +='</nav>';
-
-    /*
->>>>>>> 273bdb72c2ceaa2d52c32ad8d51db85f2506880c
     <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -53,7 +23,7 @@ function header(){
             <ul class="nav navbar-nav">
               <li><a href="#whats">iloliとは</a></li>
             </ul>
-            <a class="btn btn-success navbar-btn" href="https://docs.google.com/forms/d/1jXNnrTf5IUhFlovefWSTonEIExJvdZVld7nfWpY9tI8/viewform" role="button">お問い合わせ</a>
+            <a href="https://docs.google.com/forms/d/1jXNnrTf5IUhFlovefWSTonEIExJvdZVld7nfWpY9tI8/viewform" target=”_blank”><button type="button" class="btn btn-success btn-lg">お問い合わせ</button></a>
           </div>
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
@@ -66,27 +36,10 @@ function header(){
 
 
 function footer(){
-<<<<<<< HEAD
+
     var html = (function() {/*
     
     <!-- Footer -->
-=======
-    var html = "";
-    html +='<footer class="footer container-fluid">';
-    html +='<div class="row text-center footer-nav">';
-    html +='<div class="col-sm-4"><a href="terms.html">利用規約</a></div>';
-    html +='<div class="col-sm-4"><a href="privacy.html">プライバシーポリシー</a></div>';
-    html +='<div class="col-sm-4"><a href="https://docs.google.com/forms/d/1jXNnrTf5IUhFlovefWSTonEIExJvdZVld7nfWpY9tI8/viewform" target=”_blank”>お問い合わせ</a></div>';
-    html +='</div>';
-    html +='<div class="text-center footer-credit">';
-    html +='<p>Produced by <a href="http://creatorslab.jp" target=”_blank”><img src="img/cl_logo.png" alt="Creator \'s Lab. Logo" class="corp-logo" /></a></p>';
-    html +='</div>';
-    html +='</footer>';
-
-
-    /*
-        <!-- Footer -->
->>>>>>> 273bdb72c2ceaa2d52c32ad8d51db85f2506880c
     <footer class="footer container-fluid">
       <div class="row text-center footer-nav">
         <div class="col-sm-4"><a href="terms.html">利用規約</a></div>
@@ -103,10 +56,4 @@ function footer(){
 
     document.write(html);
 }
-<<<<<<< HEAD
 
-
-
-
-=======
->>>>>>> 273bdb72c2ceaa2d52c32ad8d51db85f2506880c
