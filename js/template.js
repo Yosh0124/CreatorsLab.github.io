0b91bdf23775824334f6cdb08d2827d63a042cd4
yuta_nishimori <hunger_yn@icloud.com>
2015-08-27 13:10:34 +0900

    お問い合わせボタンにGoogleAnalyticsの観測コードを追加

--- js/template.js
+++ js/template.js
@@ -21,7 +21,7 @@ function header(){
             <ul class="nav navbar-nav">
               <li><a href="#whats">iloliとは</a></li>
             </ul>
-            <a href="https://docs.google.com/forms/d/1jXNnrTf5IUhFlovefWSTonEIExJvdZVld7nfWpY9tI8/viewform" target=”_blank”><button type="button" class="btn btn-default navbar-btn">お問い合わせ</button></a>
+            <a href="https://docs.google.com/forms/d/1jXNnrTf5IUhFlovefWSTonEIExJvdZVld7nfWpY9tI8/viewform" target=”_blank" onclick="ga('send', 'event', 'services', 'click', 'お問い合わせ');"><button type="button" class="btn btn-default navbar-btn">お問い合わせ</button></a>
           </div>
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
@@ -42,7 +42,7 @@ function footer(){
       <div class="row text-center footer-nav">
         <div class="col-sm-4"><a href="terms.html">利用規約</a></div>
         <div class="col-sm-4"><a href="privacy.html">プライバシーポリシー</a></div>
-        <div class="col-sm-4"><a href="https://docs.google.com/forms/d/1jXNnrTf5IUhFlovefWSTonEIExJvdZVld7nfWpY9tI8/viewform">お問い合わせ</a></div>
+        <div class="col-sm-4"><a href="https://docs.google.com/forms/d/1jXNnrTf5IUhFlovefWSTonEIExJvdZVld7nfWpY9tI8/viewform" onclick="ga('send', 'event', 'services', 'click', 'お問い合わせ');">お問い合わせ</a></div>
       </div>
       <div class="text-center footer-credit">
         <p>Produced by <a href="http://creatorslab.jp" target=”_blank”><img src="img/cl_logo.png" alt="Creator \'s Lab. Logo" class="corp-logo" /></a></p>
